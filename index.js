2d81b64c3805aa2644fa14e0bdd8005c2a88f299
victorhugoMRN <164167497+victorhugoMRN@users.noreply.github.com>
2024-08-28 07:58:37 -0300

    js

---
+++ index.js
@@ -0,0 +1,26 @@
+// Usando var
+var nome = "John";
+
+// Usando let
+let idade = 25;
+
+// Usando const
+const pi = 3.14;
+
+
+let a = 5;
+let b = 10;
+let soma = a + b;
+console.log(soma);
+
+let a = 5;
+let b = 10;
+
+function multiplica (x, y) {
+    return  x * y;
+}
+
+let produto = multiplica(a, b)
+console.log(produto)
+
+let frutas = ['apple', 'banana', 'cherry'];
